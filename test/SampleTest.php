0f6ec57eccc85d56d3ebeb959e2ac75530f0cad4
morita <morita@bmj-inc.net>
2018-06-27 15:32:48 +0900

    Sample for Jenkins

--- test/SampleTest.php
+++ test/SampleTest.php
@@ -12,24 +12,8 @@ final class EmailTest extends TestCase
 {
     public function testCanBeCreatedFromValidEmailAddress()
     {
-        $this->assertInstanceOf(
-            Email::class,
-            Email::fromString('user@example.com')
-        );
-    }
-
-    public function testCannotBeCreatedFromInvalidEmailAddress()
-    {
-        $this->setExpectedException(InvalidArgumentException::class);
-
-        Email::fromString('invalid');
-    }
-
-    public function testCanBeUsedAsString()
-    {
-        $this->assertEquals(
-            'user@example.com',
-            Email::fromString('user@example.com')
-        );
+        $this->assertEquals("aaa", "aaa");
+        $this->assertTrue(true);
+        $this->assertFalse(false);
     }
 }
