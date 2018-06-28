592a416b82faed8207f9254417361790f6f04247
morita <morita@bmj-inc.net>
2018-06-28 16:52:28 +0900

    cake ver up

--- cakephp/app/Console/Command/MycmdShell.php
+++ cakephp/app/Console/Command/MycmdShell.php
@@ -12,11 +12,7 @@ class MycmdShell extends Shell
 	{
 		$test_arr = array(1, 2);
 
-		print($test_arr[3]);
-		print($test_arr[6]);
-		print($test_arr[4]);
-
-		$this->out('Error'); // 標準出力には $this->out() を利用。
+		$this->out('OK'); // 標準出力には $this->out() を利用。
 	}
 
 	function main2()
